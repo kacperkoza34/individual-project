bc71a914cef902e10e5e0ae2f0a6d70abe015a4d
Kacper Koza <kacperkoza34@gmail.com>
2019-09-07 03:48:50 +0200

    Add overlay and modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,6 +15,51 @@ document.querySelector('.sidebar-header span').addEventListener('click', functio
   else mobileMenu.classList.remove('mobile-nav-disactive');
 
   const fullMenu = document.querySelector('.aside-bar');
-  
+
   fullMenu.classList.toggle('side-bar-active');
 });
+
+/*=====================================================================================*/
+                                       /* MODAL*/
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show')
+}
+
+
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    closeModal()
+  })
+})
+
+
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal()
+  }
+})
+
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal()
+  }
+})
+
+setTimeout(function(){
+  openModal(document.getElementById('myModal'));
+},3000);
+
+
+
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show')
+  })
+  document.querySelector('#overlay').classList.add('show')
+  document.querySelector(modal).classList.add('show')
+}
