5bd9bfd9b24bc92ab2a1a11f027c7ee4ffc65f36
Kacper Koza <kacperkoza34@gmail.com>
2019-09-07 02:17:06 +0200

    Finish RWD

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,15 @@
 
 document.querySelector('.sidebar-header span').addEventListener('click', function(e) {
   e.preventDefault();
-  const mobileMenu = document.querySelector('.mobile-nav')
-  console.log(mobileMenu);
-  mobileMenu.classList.toggle('mobile-nav-active');
 
-  const fullMenu = document.querySelector('.side-bar')
-  console.log(fullMenu);
+  const mobileMenu = document.querySelector('.mobile-nav');
+
+  let condition = mobileMenu.classList.toggle('mobile-nav-active');
+
+  if(!condition) mobileMenu.classList.add('mobile-nav-disactive');
+  else mobileMenu.classList.remove('mobile-nav-disactive');
+
+  const fullMenu = document.querySelector('.aside-bar');
+  
   fullMenu.classList.toggle('side-bar-active');
-
 });
