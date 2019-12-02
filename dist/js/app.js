a31d4b128bb470f7aa1fdc88a5e2dbe0ef8db40d
Kacper Koza <kacperkoza34@gmail.com>
2019-12-02 21:08:38 +0100

    Config server v1.4

--- dist/js/app.js
+++ dist/js/app.js
@@ -22,8 +22,7 @@ initChart: function(){
 
   var ctx = document.getElementById('myChart').getContext('2d');
 
-
-  const localHost = '//' + window.location.hostname + (window.location.hostname=='protected-taiga-06834.herokuapp.com' ? ':3333' : '');
+  const localHost = '//' + window.location.hostname + (window.location.hostname=='kacper-koza-portfolio.herokuapp.com' ? ':3333' : '');
   const url = localHost + '/chart-data';
   console.log('test heroku');
 
